d758ed95d07ed8d6040a147467bb7a179b219024
Mike Evans <mike.evans@numical.com>
2016-11-28 10:07:49 +0000

    all basic tests passing

--- spec/core-spec.js
+++ spec/core-spec.js
@@ -3,36 +3,35 @@
 
 const path = require('path');
 const rimraf = require('rimraf');
-const StyleExtHtmlWebpackPlugin = require('../index.js');
-const testPlugin = require('./helpers/testPlugin.js');
-
 const version = require('./helpers/versions');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleExtHtmlWebpackPlugin = require('../index.js');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const testPlugin = require('./helpers/testPlugin.js');
 
 const OUTPUT_DIR = path.join(__dirname, '../dist');
 
-const baseConfig = () => {
+const baseConfig = (entry, cssFilename, cssLoaders) => {
+  cssFilename = cssFilename || 'styles.css';
+  cssLoaders = cssLoaders || ['css-loader'];
   return {
-    entry: path.join(__dirname, 'fixtures/one_stylesheet_with_web_font.js'),
+    entry: path.join(__dirname, `fixtures/${entry}.js`),
     output: {
       path: OUTPUT_DIR,
       filename: 'index_bundle.js'
     },
     plugins: [
       new HtmlWebpackPlugin(),
-      new ExtractTextWebpackPlugin('styles.css')
+      new ExtractTextPlugin(cssFilename),
+      new StyleExtHtmlWebpackPlugin()
     ],
     module: {
       loaders: [
         {
           test: /\.css$/,
-          loader: version.extractTextLoader(ExtractTextWebpackPlugin)
-        },
-        {
-          test: /\.woff2$/,
-          loader: 'file-loader?name=[name].[ext]'
+          loader: version.extractTextLoader(ExtractTextPlugin, cssLoaders)
         }
       ]
     }
@@ -47,7 +46,7 @@ const baseExpectations = () => {
     not: {
       html: [],
       js: [],
-      files: []
+      files: ['styles.css']
     }
   };
 };
@@ -57,37 +56,226 @@ describe(`Core functionality (webpack ${version.webpack})`, () => {
     rimraf(OUTPUT_DIR, done);
   });
 
-  it('vanilla ExtractText works', (done) => {
-    const config = baseConfig();
+  it('inlines a single stylesheet', done => {
+    const config = baseConfig('one_stylesheet');
+    const expected = baseExpectations();
+    expected.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('inlines a single stylesheet with quoted attributes and an import url', done => {
+    const config = baseConfig('one_tricky_stylesheet');
+    const expected = baseExpectations();
+    expected.html = [
+      /<style>[\s\S]*\u0040import url\(https:\/\/fonts.googleapis.com\/css\?family=Indie\+Flower[\s\S]*<\/style>/,
+      /<style>[\s\S]*colour: grey;[\s\S]*<\/style>/,
+      /<style>[\s\S]*\[contenteditable='true'\][\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('inlines multiple stylesheets from a single source', done => {
+    const config = baseConfig('two_stylesheets');
+    const expected = baseExpectations();
+    expected.html = [
+      // note British spelling
+      /<style>[\s\S]*background: snow;[\s\S]*colour: grey;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('inlines multiple stylesheets from multiple sources', done => {
+    const config = baseConfig('nested_stylesheets');
+    const expected = baseExpectations();
+    expected.html = [
+      // note British spelling
+      /<style>[\s\S]*background: snow;[\s\S]*colour: grey;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('inlining works with postcss-loader', done => {
+    const config = baseConfig('two_stylesheets', 'styles.css', ['css-loader', 'postcss-loader']);
+    const expected = baseExpectations();
+    expected.html = [
+      // note British speeling cnverted to American spelling
+      /<style>[\s\S]*background: snow;[\s\S]*color: gray;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('inlining works alongside webpack css loaders', done => {
+    const config = baseConfig('two_stylesheets');
+    config.module.loaders = [
+      {
+        test: /stylesheet1.css/,
+        loader: version.extractTextLoader(ExtractTextPlugin, ['css-loader'])
+      },
+      {
+        test: /stylesheet2.css/,
+        loader: 'style-loader!css-loader'
+      }
+    ];
+    const expected = baseExpectations();
+    // html contains fist stylesheet content but none of second
+    expected.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    expected.not.html = [
+      /<style>[\s\S]*colour: grey[/s/S]*/
+    ];
+    // js contains second stylesheet content
+    expected.js = [
+      /(colour: grey){1}/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('vanilla ExtractText works with local web font', (done) => {
+    const config = baseConfig('one_stylesheet_with_web_font');
+    config.plugins.pop(); // removes StyleExt plugin
+    config.module.loaders.push(  // add file loader for local font file
+      {
+        test: /\.woff2$/,
+        loader: 'file-loader?name=[name].[ext]'
+      }
+    );
     const expected = baseExpectations();
     expected.not.html = [
       /<style>[\s\S]*font-face[\s\S]*Indie-Flower[\s\S]*<\/style>/
-    ];
-    expected.js = [
-      /(removed by extract-text-webpack-plugin){1}/
     ];
     expected.files = [
       'styles.css',
       'Indie-Flower.woff2'
     ];
+    expected.not.files = [];
     testPlugin(webpack, config, expected, done);
   });
 
   it('works with web fonts', (done) => {
-    const config = baseConfig();
-    config.plugins.push(new StyleExtHtmlWebpackPlugin('styles.css'));
+    const config = baseConfig('one_stylesheet_with_web_font');
+    config.module.loaders.push(  // add file loader for local font file
+      {
+        test: /\.woff2$/,
+        loader: 'file-loader?name=[name].[ext]'
+      }
+    );
     const expected = baseExpectations();
     expected.html = [
       /<style>[\s\S]*font-face[\s\S]*Indie-Flower[\s\S]*<\/style>/
     ];
-    expected.js = [
-      /(removed by extract-text-webpack-plugin){1}/
-    ];
     expected.files = [
       'Indie-Flower.woff2'
     ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('plays happily with other plugins using same html plugin event', done => {
+    const config = baseConfig('one_stylesheet');
+    config.plugins.push(new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async'
+    }));
+    const expected = baseExpectations();
+    expected.html = [
+      /<script src="index_bundle.js" type="text\/javascript" async><\/script>/,
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  it('works with html webpack plugin template styles', done => {
+    const config = baseConfig('one_stylesheet');
+    // replace base HtmlWebpackPlugin
+    config.plugins[0] = new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'fixtures/html.template')
+    });
+    const expected = baseExpectations();
+    expected.html = [
+      /<style[\s\S]*background: snow;[\s\S]*<\/style>/,
+      /<style>div { background: blue }<\/style>/,
+      /<div id='template_content'>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  if (version.isWebpack1) {
+    it('works with UglifyJsPlugin to minimize css', done => {
+      const config = baseConfig('two_stylesheets');
+      config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+      const expected = baseExpectations();
+      // note spaces and unnecessary symbols have been removed
+      expected.html = [
+        /<style>[\s\S]*body{background:snow}body{colour:grey}[\s\S]*<\/style>/
+      ];
+      testPlugin(webpack, config, expected, done);
+    });
+  }
+
+  if (version.isWebpack2) {
+    it('works with LoaderOptionsPlugin to minimize css', done => {
+      const config = baseConfig('two_stylesheets');
+      config.plugins.push(new webpack.LoaderOptionsPlugin({minimize: true}));
+      const expected = baseExpectations();
+      // note spaces and unnecessary symbols have been removed
+      expected.html = [
+        /<style>[\s\S]*body{background:snow}body{colour:grey}[\s\S]*<\/style>/
+      ];
+      testPlugin(webpack, config, expected, done);
+    });
+  }
+
+  it('vanilla ExtractText handles [name] and [id] in css filename', done => {
+    const config = baseConfig('one_stylesheet', '[name][id].css');
+    config.plugins.pop(); // remove StyleExt
+    const expected = baseExpectations();
+    expected.not.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    // default chunk name and id
+    expected.files = [
+      'main0.css'
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  // Note: why on earth test this? For people who simply add StyleExt to an existing 
+  // configuration with ExtractTextWebpackPlugin already in it
+  it('handles [name] and [id] in css filename', done => {
+    const config = baseConfig('one_stylesheet', '[name][id].css');
+    const expected = baseExpectations();
+    expected.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
     expected.not.files = [
-      'styles.css'
+      'main0.css'
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  // Note: why on earth test this? For people who simply add StyleExt to an existing 
+  // configuration with ExtractTextWebpackPlugin already in it
+  it('handles [contenthash] in css filename', done => {
+    const config = baseConfig('one_stylesheet', '[contenthash].css');
+    const expected = baseExpectations();
+    expected.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    testPlugin(webpack, config, expected, done);
+  });
+
+  // Note: why on earth test this? For people who simply add StyleExt to an existing 
+  // configuration with ExtractTextWebpackPlugin already in it
+  it('handles nested css filename', done => {
+    const config = baseConfig('one_stylesheet', '/css/styles.css');
+    const expected = baseExpectations();
+    expected.html = [
+      /<style>[\s\S]*background: snow;[\s\S]*<\/style>/
+    ];
+    expected.not.files = [
+      'styles.css',
+      'css/styles.css'
     ];
     testPlugin(webpack, config, expected, done);
   });
--- spec/helpers/versions.js
+++ spec/helpers/versions.js
@@ -4,20 +4,24 @@ const setModuleVersion = require('dynavers')('dynavers.json');
 
 const VERSIONS = {
   webpack1: {
+    isWebpack1: true,
+    isWebpack2: false,
     webpack: '1.13.3',
     extractText: '1.0.1',
-    extractTextLoader: (ExtractTextPlugin) => {
-      return ExtractTextPlugin.extract('style-loader', 'css-loader');
+    extractTextLoader: (extractTextPlugin, cssLoaders) => {
+      return extractTextPlugin.extract('style-loader', cssLoaders);
     }
   },
   webpack2: {
+    isWebpack1: false,
+    isWebpack2: true,
     webpack: '2.1.0-beta.27',
     extractText: '2.0.0-beta.4',
-    extractTextLoader: (ExtractTextPlugin) => {
-      return ExtractTextPlugin.extract({
+    extractTextLoader: (extractTextPlugin, cssLoaders) => {
+      return extractTextPlugin.extract({
         fallbackLoader: 'style-loader',
-        loader: 'css-loader'}
-      );
+        loader: cssLoaders
+      });
     }
   }
 };
@@ -29,5 +33,6 @@ if (selected) {
 } else {
   throw new Error(`Unknown webpack version '${process.env.VERSION}'`);
 }
+
 module.exports = selected;
 
